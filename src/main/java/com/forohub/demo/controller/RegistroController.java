7cd4de7c9fe8148fa3bc7e7c1fa7c4e47efee639
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-08 15:37:30 -0600

    Se agregaron los metodos a los controllers

--- src/main/java/com/forohub/demo/controller/RegistroController.java
+++
@@ -1,9 +0,0 @@
-package com.forohub.demo.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/registro")
-public class RegistroController {
-}
