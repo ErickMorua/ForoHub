5348d1cf9bf417d87a4ea78332dd9fbf624a15eb
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-09 16:53:45 -0600

    agregando metodo validar

---
+++ src/main/java/com/forohub/demo/domain/topico/validaciones/UsuarioExistente.java
@@ -0,0 +1,21 @@
+package com.forohub.demo.domain.topico.validaciones;
+
+import com.forohub.demo.domain.topico.CrearTopicoDTO;
+import com.forohub.demo.domain.usuario.UsuarioRpository;
+import jakarta.validation.ValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UsuarioExistente implements ValidarTopico {
+
+    @Autowired
+    private UsuarioRpository usuarioRpository;
+
+    @Override
+    public void validar(CrearTopicoDTO datos) {
+        var usuario = usuarioRpository.existsById(datos.idAutor());
+
+        if (!usuario) {
+            new ValidationException("Autor no registrado");
+        }
+    }
+}
