70fdbd4807258a239a8a00f16965c159ed5bf073
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-09 16:37:49 -0600

    creando metodo validar

---
+++ src/main/java/com/forohub/demo/domain/curso/validaciones/ValidarCurso.java
@@ -0,0 +1,7 @@
+package com.forohub.demo.domain.curso.validaciones;
+
+import com.forohub.demo.domain.curso.CrearCursoDTO;
+
+public interface ValidarCurso {
+    public void validar(CrearCursoDTO datos);
+}
