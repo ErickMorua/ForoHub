fe0d29d2e33a8c3872dfcc2f1d5cb4ab0ce7569f
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-09 16:45:31 -0600

    creando interface de validador topico

---
+++ src/main/java/com/forohub/demo/domain/topico/validaciones/ValidarTopico.java
@@ -0,0 +1,7 @@
+package com.forohub.demo.domain.topico.validaciones;
+
+import com.forohub.demo.domain.topico.CrearTopicoDTO;
+
+public interface ValidarTopico {
+    public void validar(CrearTopicoDTO datos);
+}
