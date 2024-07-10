daa835f891f9d8474f555280f9d531e93acafb5b
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-10 12:20:17 -0600

    agregando AutenticacionController para manejo de autenticación de usuarios

--- src/main/java/com/forohub/demo/controller/AutenticacionController.java
+++ src/main/java/com/forohub/demo/controller/AutenticacionController.java
@@ -2,13 +2,14 @@ package com.forohub.demo.controller;
 
 import com.forohub.demo.domain.usuario.AutenticacionUsuarioDTO;
 import com.forohub.demo.domain.usuario.Usuario;
+import com.forohub.demo.infra.security.JWTTokenDTO;
+import com.forohub.demo.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
