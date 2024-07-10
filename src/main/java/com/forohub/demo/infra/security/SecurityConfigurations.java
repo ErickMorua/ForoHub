7994252bd37aec978655937a12ec92c252d5736a
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-09 18:08:45 -0600

    agregando clase para autenticación basada en token JWT

---
+++ src/main/java/com/forohub/demo/infra/security/SecurityConfigurations.java
@@ -0,0 +1,13 @@
+package com.forohub.demo.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+}
