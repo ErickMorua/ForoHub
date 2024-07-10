5ef9117a7cca410436965f5cd5ab8f0c7f984805
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-10 12:42:51 -0600

    Agregando interfaz ValidarRespuesta para validación de respuestas

---
+++ src/main/java/com/forohub/demo/domain/topico/respuesta/validaciones/ValidarRespuesta.java
@@ -0,0 +1,7 @@
+package com.forohub.demo.domain.topico.respuesta.validaciones;
+
+import com.forohub.demo.domain.topico.respuesta.CrearRespuestaDTO;
+
+public interface ValidarRespuesta {
+    public void validar(CrearRespuestaDTO datos);
+}
