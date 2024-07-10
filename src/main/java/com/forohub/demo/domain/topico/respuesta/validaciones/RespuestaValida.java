91a14b0e7ca70e3aceadb78483f1a52868a1a527
ErickMorua <161556808+ErickMorua@users.noreply.github.com>
2024-07-10 12:47:53 -0600

    Agregando RespuestaValida para validación de integridad de respuestas

---
+++ src/main/java/com/forohub/demo/domain/topico/respuesta/validaciones/RespuestaValida.java
@@ -0,0 +1,35 @@
+package com.forohub.demo.domain.topico.respuesta.validaciones;
+
+import com.forohub.demo.domain.topico.TopicoRepository;
+import com.forohub.demo.domain.topico.respuesta.CrearRespuestaDTO;
+import com.forohub.demo.domain.topico.respuesta.RespuestaRepository;
+import com.forohub.demo.domain.usuario.UsuarioRpository;
+import com.forohub.demo.infra.errores.ValidacionDeIntegridad;
+import jakarta.validation.ValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RespuestaValida implements ValidarRespuesta {
+
+    @Autowired
+    RespuestaRepository respuestaRepository;
+
+    @Autowired
+    UsuarioRpository usuarioRepository;
+
+    @Autowired
+    TopicoRepository topicoRepository;
+
+    @Override
+    public void validar(CrearRespuestaDTO datos) {
+        if (datos.mensaje() == null) {
+            throw new ValidacionDeIntegridad("Mensaje no encontrado");
+        }
+
+        if (respuestaRepository.existsByTopicoAndMensajeAndAutorRespuesta(
+                topicoRepository.getReferenceById(datos.idTopico()),
+                datos.mensaje(),
+                usuarioRepository.getReferenceById(datos.idAutor()))) {
+            throw new ValidationException("Ya existe una respuesta en el topico")
+        }
+    }
+}
